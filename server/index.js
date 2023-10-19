f66aa8a4b9ce46b03397b8f3bfa5d1a107716c3b
farazakram123 <mfarazakram0786@gmail.com>
2023-10-19 19:13:53 +0530

    Created a demo express server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("<h1> This is Backend HomePage</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is runnnig at port ${PORT}`);
+});
