aaa2bbe223368b68bc608ba38ad3ea606aa190d6
farazakram123 <mfarazakram0786@gmail.com>
2025-09-07 23:15:00 +0530

    feat: Initialize React application with Vite and basic routing
    - Created index.html as the entry point for the application.
    - Updated package.json to include necessary dependencies and scripts for development.
    - Created individual components for Home, Login, SignUp, and Dashboard with corresponding CSS files.
    - Set up main entry point (main.jsx) for rendering the application with React Router.

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home/Home'
+import Dashboard from './components/Dashboard/Dashboard'
+import Login from './components/Login/Login'
+import SignUp from './components/SignUp/SignUp'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+  )
+}
---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Dashboard.css'
+const Dashboard = () => {
+    return (
+        <div>
+            DashBoard
+        </div>
+    )
+}
+
+export default Dashboard
